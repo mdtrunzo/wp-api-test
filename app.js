1d5f3c7becaf6bf73c0d4cb5b4d4f2a8d7f677d2
Matias Daniel Trunzo <matiasdanieltrunzo@MacBook-Pro-de-Matias.local>
2022-09-08 14:43:27 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const session = require('express-session')
+const PORT = process.env.PORT
+const cors = require('cors')
+
+const app = express()
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
+  })
+)
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use(
+  session({
+    secret: 'cats',
+    resave: false,
+    saveUninitialized: false,
+  })
+)
+
+let indexRoutes = require('./routes/index.js')
+
+//Serve frontend
+if (process.env.NODE_ENV == 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+  })
+} else {
+  app.use('/', indexRoutes)
+}
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
---
+++ routes/index.js
@@ -0,0 +1,37 @@
+'use strict'
+require('dotenv').config()
+const router = require('express').Router()
+
+router.get('/', (req, res) => {
+  res.send('HOLAAAA')
+})
+
+router.get('/webhook', (req, res) => {
+  try {
+    console.log('GET: Someone is pinging me!')
+
+    let mode = req.query['hub.mode']
+    let token = req.query['hub.verify_token']
+    let challenge = req.query['hub.challenge']
+
+    if (mode && token && mode === 'subscribe' && 'TestingTokenWP' === token) {
+      return res.status(200).send(challenge)
+    } else {
+      return res.sendStatus(403)
+    }
+  } catch (error) {
+    console.error({ error })
+    return res.sendStatus(500)
+  }
+})
+
+router.post('/webhook', async (req, res) => {
+  try {
+    console.log('POST: Someone is pinging me!')
+    return res.sendStatus(200)
+  } catch (error) {
+    console.error({ error })
+    return res.sendStatus(500)
+  }
+})
+module.exports = router
